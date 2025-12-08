a720565c6edcf863ed133b1627ae785ec6117201
Joanne-cmd <joanne.m.muthoni@gmail.com>
2025-12-08 10:04:43 +0000

    always start guest agent
    Signed-off-by: Joanne-cmd <joanne.m.muthoni@gmail.com>

--- packages/guest-agent/entrypoint.sh
+++ packages/guest-agent/entrypoint.sh
@@ -4,10 +4,10 @@ ip addr
 socat VSOCK-LISTEN:8080,fork TCP:127.0.0.1:8080 &
 socat VSOCK-LISTEN:8022,fork TCP:127.0.0.1:22 &
 echo "CMIO guest agent setting: $CMIO_GUEST_AGENT" >> /dev/console
-if [ x$CMIO_GUEST_AGENT != x ]; then
+#if [ x$CMIO_GUEST_AGENT != x ]; then
 		echo "Starting guest agent" >> /dev/console
         RUST_LOG=trace /bin/guest-agent >> /dev/console 2>&1
-fi
+#fi
 echo "Guest agent failed to start" >> /dev/console
 while true; do
         sleep 99999
