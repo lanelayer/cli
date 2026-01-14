e695e5165709bf19f63a73a91b70bae0258378a4
Joanne-cmd <joanne.m.muthoni@gmail.com>
2026-01-14 07:15:56 +0000

    fix: guest agent
    Signed-off-by: Joanne-cmd <joanne.m.muthoni@gmail.com>

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,4 +1,4 @@
 export const LANE_SNAPSHOT_BUILDER_IMAGE =
   "ghcr.io/lanelayer/lane-snapshot-builder@sha256:8c9e5a2838040be2c8979dfc197292cbfa46f9944f29b8af84c98edcba8f7ca0";
 export const GUEST_AGENT_IMAGE =
-  "ghcr.io/lanelayer/lane-guest-agent@sha256:2824cb1f5f6d68b7292dd6d542267061287b03e6d5f16ebdbf43ff7748970101";
+  "ghcr.io/lanelayer/lane-guest-agent@sha256:efed94419b7ee1ee762f42fb55eb1700186d89fdf01f1d9f789d61bc151a119b";
