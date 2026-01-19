a03f6f9ab25d98dd2aefc9bf938fb31203b84541
Michael Asiedu <michaelasiedu148@gmail.com>
2026-01-19 21:08:55 +0000

    chore: bump version to 0.4.9 and update lane-snapshot-builder image digest

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,4 +1,4 @@
 export const LANE_SNAPSHOT_BUILDER_IMAGE =
-  "ghcr.io/lanelayer/lane-snapshot-builder@sha256:c221b2d9ad0bda5aafe2e05658e6b4b44f4b3370f6d6c12dbbdb20179eefaf60";
+  "ghcr.io/lanelayer/lane-snapshot-builder@sha256:5de1cfaea1a33c8cdcee1abd3306ae9a25709a2522fa33c95822a4fc209b7a18";
 export const GUEST_AGENT_IMAGE =
   "ghcr.io/lanelayer/lane-guest-agent@sha256:efed94419b7ee1ee762f42fb55eb1700186d89fdf01f1d9f789d61bc151a119b";
